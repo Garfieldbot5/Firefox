0b8ac95e05c4d416f81fbb13565b1ace3ac74f10
Garfieldbot5 <109533758+Garfieldbot5@users.noreply.github.com>
2026-02-07 01:15:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,25 +18,34 @@ async function startBot() {
   
   sock.ev.on('creds.update', saveCreds)
 
-  sock.ev.on('connection.update', (update) => {
+  sock.ev.on('connection.update', async (update) => {
   const { connection, lastDisconnect } = update
 
   if (connection === 'open') {
     console.log('✅ WhatsApp connected')
+
+    if (!pairedOnce) {
+      pairedOnce = true
+
+      const myJid = sock.user?.id
+if (myJid) {
+  await sock.sendMessage(myJid, {
+    image: { url: 'https://i.postimg.cc/SKWWycnC/2f219c4e-35ba-41b3-bb11-91fdfe78291f.jpg' },
+    caption: '✅ Bot successfully paired & connected 🎉'
+  })
+}
+    }
   }
 
   if (connection === 'close') {
-    console.log('❌ Connection closed')
-
-    
     if (lastDisconnect?.error?.output?.statusCode !== 401) {
       console.log('🔄 Reconnecting...')
       startBot()
     } else {
       console.log('⚠️ Logged out. Scan QR again.')
+      pairedOnce = false
     }
   }
 })
-
 
 startBot()
